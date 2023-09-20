bda018270aaf1f15424703e8cc824bdf51e1c7e1
Erica <140522086+erica123-source@users.noreply.github.com>
2023-09-19 22:10:07 -0300

    primer mensaje

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import Boton from "./componentes/Boton";
+import Contador from "./componentes/Contador";
+import logoContador from "./imagenes/imagen-contador.jpg";
+import { useState } from "react";
+
+function App() {
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+  };
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+  };
+
+  return (
+    <div className="App">
+      <div className="logo-contenedor">
+        <img
+          className="img-logo-contenedor"
+          src={logoContador}
+          atl="logo-de-contador"
+        />
+      </div>
+      <div className="contenedor-principal">
+        <Contador numClics={numClics} />
+        <Boton texto="Clic" esBotonDeClic={true} manejarClic={manejarClic} />
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
